c049b6d5ba6d0983b57b562ef430d8afefebcea1
JanssenYang <944386630@qq.com>
2020-04-08 19:18:41 -0400

    remove static search

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 
 const eventRoutes = require('./events');
 const userRoutes = require('./users');
+const searchRoutes = require('./search');
 const path = require('path');
 
 const constructorMethod = (app) => {
-    app.use('/search', (req, res)=>{
-        res.sendFile(path.resolve('static/search.html'));
-    });
+    app.use('/search', searchRoutes);
 	app.use('/events', postRoutes);
     app.use('/users', userRoutes);
     app.use('/', (req,res)=>{
