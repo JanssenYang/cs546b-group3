e8ea5a3f6445a0ce9b658a6ded03a96a83e4b593
Nick Primamore <lancernp@gmail.com>
2020-04-08 15:55:06 -0400

    Initial setup with all folders and mongo collections

---
+++ data/events.js
@@ -0,0 +1,4 @@
+const mongoCollections = require('../config/mongoCollections');
+const users = mongoCollections.users;
+const events = mongoCollections.events;
+const { ObjectId } = require('mongodb');
---
+++ data/index.js
@@ -0,0 +1,7 @@
+const userData = require("./users");
+const eventData = require("./events");
+
+module.exports = {
+    users: userData,
+    events: eventData
+};
