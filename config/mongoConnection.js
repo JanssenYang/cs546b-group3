2c4f72970d6e5c686567ed69cc91bedd9409888d
lking2 <32401256+lking2@users.noreply.github.com>
2020-04-08 21:28:19 -0400

    Removed spaces from database name

--- config/mongoConnection.js
+++ config/mongoConnection.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 const settings = {
   mongoConfig: {
     serverUrl: 'mongodb://localhost:27017/',
-    database: 'Schedule Sharing Web Application'
+    database: 'ScheduleSharingWebApplication'
   }
 };
 const mongoConfig = settings.mongoConfig;
