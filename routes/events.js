72f0b769b9f494a957f3b9f1665104828394ba61
Nick Primamore <lancernp@gmail.com>
2020-05-03 02:48:10 -0400

    Added basic single event page and the route for it in routes/events.js

--- routes/events.js
+++ routes/events.js
@@ -2,7 +2,28 @@ const express = require('express');
 const router = express.Router();
 const data = require('../data');
 const eventData = data.events;
+const userData = data.users;
+const path = require('path')
 
-
+//Single event page
+router.get('/:id', async (req, res) => {
+    const event = await eventData.getEvent(req.params.id);
+    const participants = event.participants;
+    let participantObjects = [];
+    for(let index = 0; index < participants.length; index++){
+        let participant = await userData.getUserById(participants[index]);
+        let participantObject = {
+            userName: participant.userName,
+            name: participant.firstName + " " + participant.lastName
+        }
+        participantObjects.push(participantObject);
+    }
+    res.render('events/singleEvent', 
+        {
+            event: event,
+            numParticipants: participants.length,
+            participants: participantObjects
+        });
+});
 
 module.exports = router;
